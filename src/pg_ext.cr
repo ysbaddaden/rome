38ecd560ebd33d2f12f38261fbff337aa8169412
Julien Portalier <julien@portalier.com>
2019-09-30 18:00:23 +0200

    Fix: encode/encode UUID type (PostgreSQL)

--- src/pg_ext.cr
+++ src/pg_ext.cr
@@ -2,16 +2,33 @@ require "pg"
 require "uuid"
 require "uuid/json"
 
-struct PG::Decoders::UuidDecoder
-  def decode(io, bytesize)
-    bytes = uninitialized UInt8[16]
-    io.read(bytes.to_slice)
-    UUID.new(bytes)
+module PG
+  module Decoders
+    struct UUIDDecoder
+      include Decoder
+
+      def_oids [
+        2950, # uuid
+      ]
+
+      def decode(io, bytesize, oid)
+        bytes = uninitialized UInt8[16]
+        io.read(bytes.to_slice)
+        UUID.new(bytes)
+      end
+
+      def type
+        UUID
+      end
+    end
+
+    # replaces the original UUID decoder that returns a string:
+    register_decoder UUIDDecoder.new
   end
 end
 
 struct PQ::Param
   def self.encode(val : UUID)
-    encode val.bytes
+    encode Bytes.new(val.to_unsafe, 16).dup
   end
 end
